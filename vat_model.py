9c2fb3b46d1b5909efca3de1fdc3be804e6c2099
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2021-11-28 17:37:37 +0100

    Xi Fix

--- vat_model.py
+++ vat_model.py
@@ -5,7 +5,7 @@ from tensorflow.keras import layers
 
 
 class VAT(keras.Model):
-    def __init__(self, input_shape=(28,28,1), num_classes=10, alpha=1, eps=10**-6):
+    def __init__(self, input_shape=(28, 28, 1), num_classes=10, alpha=1, eps=10 ** -6):
         super(VAT, self).__init__()
         self.model = keras.Sequential(
             [
@@ -25,6 +25,7 @@ class VAT(keras.Model):
         self.flatten = layers.Flatten()
         self.eps = eps
         self.alpha = alpha
+        self.xi = 10**-6
 
         self.loss_tracker = keras.metrics.Mean(name="loss")
         self.accuracy = keras.metrics.Accuracy(name="accuracy")
@@ -32,7 +33,7 @@ class VAT(keras.Model):
     def train_step(self, data):
         # Unpack the data. Its structure depends on your model and
         # on what you pass to `fit()`.
-        (x_l, x_ul), (y_l, ) = data
+        (x_l, x_ul), (y_l,) = data
 
         with tf.GradientTape() as tape:
             y_l_pred = self.model(x_l)  # Forward pass
@@ -42,11 +43,11 @@ class VAT(keras.Model):
             loss = self.cross_entropy(y_l, y_l_pred)
             d = tf.random.normal(tf.shape(x_ul))
             d = d / tf.linalg.norm(self.flatten(d))
-            r = self.eps * d
+            r = self.xi * d
             y_vadv = self.model(x_ul + r)
             temp = self.kl(y_ul_pred, y_vadv)
             g = tf.gradients(temp, r, stop_gradients=[r])[0]
-            r_vadv = g / tf.linalg.norm(self.flatten(g))
+            r_vadv = self.eps * g / tf.linalg.norm(self.flatten(g))
             r_vadv = tf.stop_gradient(r_vadv)
             loss += self.alpha * self.kl(y_vadv, self.model(x_ul + r_vadv))
 
